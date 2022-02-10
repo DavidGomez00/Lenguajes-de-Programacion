5e77ec1980171b3eb554645ea56443ab99b854e3
David Gómez Ortiz <dvdgmzrtz@gmail.com>
2022-02-10 17:09:21 +0100

    First commit
    Archivos de Domingo

---
+++ Lexer.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+from sly import Lexer
+import os
+import re
+import sys
+
+
+
+
+
+class CoolLexer(Lexer):
+    tokens = {OBJECTID, INT_CONST, BOOL_CONST, TYPEID,
+              ELSE, IF, FI, THEN, NOT, IN, CASE, ESAC, CLASS,
+              INHERITS, ISVOID, LET, LOOP, NEW, OF,
+              POOL, THEN, WHILE, NUMBER, STR_CONST, LE, DARROW, ASSIGN}
+    #ignore = '\t '
+    literals = {}
+    # Ejemplo
+    ELSE = r'\b[eE][lL][sS][eE]\b'
+
+    CARACTERES_CONTROL = [bytes.fromhex(i+hex(j)[-1]).decode('ascii')
+                          for i in ['0', '1']
+                          for j in range(16)] + [bytes.fromhex(hex(127)[-2:]).decode("ascii")]
+
