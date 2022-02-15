e2b1f734a5715c90049e13323943723d3a099a14
Juan <9juanbc9@gmail.com>
2022-02-15 15:37:50 +0100

    cambios

--- Lexer.py
+++ Lexer.py
@@ -14,14 +14,26 @@ class CoolLexer(Lexer):
               ELSE, IF, FI, THEN, NOT, IN, CASE, ESAC, CLASS,
               INHERITS, ISVOID, LET, LOOP, NEW, OF,
               POOL, THEN, WHILE, NUMBER, STR_CONST, LE, DARROW, ASSIGN}
-    #ignore = '\t '
+    ignore = '\t '
     literals = {}
     # Ejemplo
     ELSE = r'\b[eE][lL][sS][eE]\b'
+    WHILE = r'\b[Ww][Hh][Ii][Ll][Ee]\b'
+    INT_CONST = r'\b[0-9]+\b'
 
-    @_(r'\b[Ww][Hh][Ii][Ll][Ee]\b')
+    @_(r'\bt[Rr][Uu][Ee]\b')
+    def BOOL_CONST(self, t):
+        t.value = (t.value).lower()
+        return t
+    
+    @_(r'\bf[Aa][Ll][Ss][Ee]\b')
+    def BOOL_CONST(self, t):
+        t.value = (t.value).lower()
+        return t
+
+    @_(r'\b[A-Z]+\b')
     def TYPEID(self, t):
-        t.value = (t.value) + 'dddd'
+        t.value = (t.value)
         return t
 
     CARACTERES_CONTROL = [bytes.fromhex(i+hex(j)[-1]).decode('ascii')
@@ -52,7 +64,3 @@ class CoolLexer(Lexer):
             
             list_strings.append(result)
         return list_strings
-
-
-c = CoolLexer()
-c.salida("while")
