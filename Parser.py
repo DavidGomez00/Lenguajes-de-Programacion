d2cbd470c16da1bfe065f3a20763f3379605d68e
Juan <9juanbc9@gmail.com>
2022-03-31 18:31:38 +0200

    asdf

--- Parser.py
+++ Parser.py
@@ -13,4 +13,69 @@ class CoolParser(Parser):
     debugfile = "salida.out"
     errores = []
 
-    pass
+    # Program
+    @_('_class')
+    def _program(self, p):
+      return Programa(secuencia=[p._class])
+
+    @_('_class _program')
+    def _program(self, p):
+      return Programa(secuencia=[p._class]+p._program.secuencia)
+
+    # Class
+    @_('CLASS TYPEID "{" _feature_list "}"')
+    def _class(self, p):
+      return Clase(nombre=p.TYPEID,
+                   padre="Object",
+                   nombre_fichero=self.nombre_fichero,
+                   caracteristicas=p[3])
+
+    @_('CLASS TYPEID inherits TYPEID "{" _feature_list "}"')
+    def _class(self, p):
+      return Clase(nombre=p[1],
+                   padre=p[3],
+                   nombre_fichero=self.nombre_fichero,
+                   caracteristicas=p[5])
+
+    # Feature_list
+    @_('_feature ";" _feature_list')
+    def _feature_list(self, p):
+      return p._feature
+
+    @_('')
+    def _feature_list(self, p):
+      return []
+
+    
+    # Expr
+    @_('BOOL_CONST')
+    def _expr(self, p):
+      return Expresion(valor=self.valor)
+
+    @_('STR_CONST')
+    def _expr(self, p):
+      return String(valor=self.valor)
+
+    @_('INT_CONST')
+    def _expr(self, p):
+      return Integer(valor=self.valor) 
+
+    @_('OBJECTID')
+    def _expr(self, p):
+      return NoExpr(nombre=self.valor)
+
+    @_('"(" _expr ")"')
+    def _expr(self, p):
+      return Expresion(cast='(' + p.[1] + ')')
+
+    @_('_expr "=" _expr')
+    def _expr(self, p):
+      return Expresion()
+
+    @_('_expr "<" _expr')
+    def _expr(self, p):
+      return Expresion()
+
+    @_('NOT _expr')
+    def _expr(self, p):
+      return Neg(expresion=p.[1])
