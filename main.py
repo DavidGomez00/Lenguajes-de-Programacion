b0aa69da435e01c31871434b336050283cea69ef
David Gómez Ortiz <dvdgmzrtz@gmail.com>
2022-05-05 18:17:22 +0200

    fixed

--- main.py
+++ main.py
@@ -68,7 +68,6 @@ if True:
             parser.errores = []
             bien = ''.join([c for c in g.readlines() if c and '#' not in c])
             g.close()
-            # ERROR aqui
             j = parser.parse(lexer.tokenize(entrada))
             try:
                 # que es "not parser.errores"
@@ -81,6 +80,7 @@ if True:
                 if resultado.lower().strip().split() != bien.lower().strip().split():
                     print(f"Revisa el fichero {fich}")
                     if DEBUG:
+                        '''
                         nuestro = [linea for linea in resultado.split('\n') if linea]
                         bien = [linea for linea in bien.split('\n') if linea]
                         linea = 0
@@ -88,6 +88,7 @@ if True:
                             linea += 1
                         print(colored('\n'.join(nuestro[linea:linea+NUMLINEAS]), 'white', 'on_red'))
                         print(colored('\n'.join(bien[linea:linea+NUMLINEAS]), 'blue', 'on_green'))
+                        '''
                         f = open(os.path.join(DIR, fich)+'.nuestro', 'w')
                         g = open(os.path.join(DIR, fich)+'.bien', 'w')
                         f.write(resultado.strip())
@@ -96,4 +97,6 @@ if True:
                         g.close()
             except Exception as e:
                 print(f"Lanza excepción en {fich} con el texto {e}")
+                import traceback
+                traceback.print_exc()
 
